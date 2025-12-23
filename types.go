0cccf5bcc336cb8c4a3713cf14c33ff233a8c57a
l00pss <mammadli.vugar@hotmail.com>
2025-12-23 15:44:28 +0400

    Refactor WAL structure: move State, Status, Cursor, Batch, and batchEntry types to types.go

--- types.go
+++ types.go
@@ -5,6 +5,69 @@ import (
 	"time"
 )
 
+type State int
+
+const (
+	// StateInitializing indicates that the WAL is initializing.
+	Initializing State = iota
+	// StateReady indicates that the WAL is ready for operations.
+	Ready
+	// StateClosed indicates that the WAL has been closed.
+	Closed
+)
+
+type Status int
+
+const (
+	// OK indicates that the operation was successful.
+	OK Status = iota
+	// Corrupted indicates that the WAL is corrupted.
+	Corrupted
+)
+
+type Cursor struct {
+	FirstIndex uint64
+	LastIndex  uint64
+}
+
+func (c *Cursor) IsValid() bool {
+	return c.FirstIndex <= c.LastIndex
+}
+
+func StartCursor() Cursor {
+	return Cursor{
+		FirstIndex: 0,
+		LastIndex:  0,
+	}
+}
+
+type Batch struct {
+	entries []batchEntry
+	datas   []byte
+}
+
+type batchEntry struct {
+	index uint64
+	size  int
+}
+
+func (b *Batch) Reset() {
+	b.entries = b.entries[:0]
+	b.datas = b.datas[:0]
+}
+
+func (b *Batch) Len() int {
+	return len(b.entries)
+}
+
+func (b *Batch) Write(index uint64, data []byte) {
+	b.entries = append(b.entries, batchEntry{
+		index: index,
+		size:  len(data),
+	})
+	b.datas = append(b.datas, data...)
+}
+
 type Entry struct {
 	Index     uint64 // Monotonically increasing index
 	Term      uint64 // For consensus algorithms like Raft
--- wal.go
+++ wal.go
@@ -9,74 +9,10 @@ import (
 	"github.com/l00pss/littlecache"
 )
 
-type State int
-
-const (
-	// StateInitializing indicates that the WAL is initializing.
-	Initializing State = iota
-	// StateReady indicates that the WAL is ready for operations.
-	Ready
-	// StateClosed indicates that the WAL has been closed.
-	Closed
-)
-
-type Status int
-
-const (
-	// OK indicates that the operation was successful.
-	OK Status = iota
-	// Corrupted indicates that the WAL is corrupted.
-	Corrupted
-)
-
-type Cursor struct {
-	FirstIndex  uint64
-	LastIndex   uint64
-}
-
-func (c *Cursor) IsValid() bool {
-	return c.FirstIndex <= c.LastIndex
-}
-
-func StartCursor() Cursor {
-	return Cursor{
-		FirstIndex: 0,
-		LastIndex:  0,
-	}
-}
-
-type Batch struct {
-	entries []batchEntry
-	datas   []byte
-}
-
-type batchEntry struct {
-	index uint64
-	size  int
-}
-
-func (b *Batch) Reset() {
-	b.entries = b.entries[:0]
-	b.datas = b.datas[:0]
-}
-
-func (b *Batch) Len() int {
-	return len(b.entries)
-}
-
-
-func (b *Batch) Write(index uint64, data []byte) {
-	b.entries = append(b.entries, batchEntry{
-		index: index,
-		size:  len(data),
-	})
-	b.datas = append(b.datas, data...)
-}
-
 type WAL struct {
 	mu             sync.RWMutex
-	state 		   State
-	status 	       Status
+	state          State
+	status         Status
 	cusror         Cursor
 	dir            string
 	tailSFH        os.File
